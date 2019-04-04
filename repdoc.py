096f47f3fb24cf08848afe24a8c369734f17f100
Nicolás Cardiel <cardiel@ucm.es>
2019-04-04 13:23:12 +0200

    New version of repdoc_bitacora.html

--- repdoc.py
+++ repdoc.py
@@ -21,6 +21,7 @@ COLOR_TITULACIONES = '#AF4CAF'
 COLOR_ASIGNATURAS = '#AF4C50'
 COLOR_PROFESORES = '#4CAF50'
 COLOR_NO_DISPONIBLE = '#555'
+COLOR_BITACORA = '#4CAFAF'
 
 
 def new_uuid(megalist_uuid):
@@ -646,6 +647,10 @@ def export_to_html_titulaciones(tabla_titulaciones):
 <p>Enlace a tabla de 
 <a href="repdoc_asignacion.html">asignación de asignaturas</a></p>
 <p></p>
+<p>Enlace al
+<a href="repdoc_bitacora.html">cuaderno de bitácora</a>
+del reparto docente</p>
+<p></p>
 <h2>Tabla resumen de titulaciones</h2>
 ''')
 
@@ -1222,8 +1227,104 @@ def export_to_html_bitacora(bitacora):
 
     """
 
-    bitacora.to_html('repdoc_bitacora.html')
     bitacora.to_excel('repdoc_bitacora.xlsx', header=True)
+
+    bitacora.to_html('repdoc_bitacora_raw.html')
+
+    f = open('repdoc_bitacora.html', 'wt')
+    f.write('''
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Bitácora</title>
+
+  <style>
+
+  p, h1, h2, h3 {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  #tabla_bitacora {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+  }
+
+  #tabla_bitacora td, #tabla_bitacora th {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+
+  #tabla_bitacora tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  #tabla_bitacora tr:hover {
+    background-color: #ddd;
+  }
+
+  #tabla_bitacora th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: ''' + COLOR_BITACORA + ''';
+    color: white;
+  }
+
+  </style>
+</head>
+
+<body>
+
+''')
+
+    f.write('''
+<h1>Reparto Docente FTA, curso 2019-2020</h1> 
+<h2>Cuaderno de bitácora</h2>
+''')
+
+    f.write('''
+<table id="tabla_bitacora">
+
+<thead>
+<tr style="text-align: left;">
+''')
+
+    f.write('<th>uuid_bita</th>\n')
+    for colname in bitacora.columns.tolist():
+        f.write('<th>' + colname + '</th>\n')
+
+    f.write('''
+</tr>
+</thead>
+
+<tbody>
+
+''')
+
+    for uuid_bita in bitacora.index:
+        status = str(bitacora.loc[uuid_bita]['date_removed']) == 'None'
+        if status:
+            f.write('\n<tr>\n')
+        else:
+            f.write(
+                '\n<tr style="background: ' + COLOR_NO_DISPONIBLE + ';">\n'
+            )
+        f.write('<th>{}</th>\n'.format(uuid_bita))
+        for colname in bitacora.columns.tolist():
+            f.write('<td>{}</td>\n'.format(
+                bitacora.loc[uuid_bita][colname]
+            ))
+        f.write('</tr>\n')
+
+    f.write('\n</tbody>\n\n')
+    f.write('\n</table>\n\n')
+
+    f.write(date_last_update())
+    f.write('</body>\n\n</html>\n')
+    f.close()
 
 
 def main(args=None):
