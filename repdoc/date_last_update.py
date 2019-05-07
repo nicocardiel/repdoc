461858d24a1ad1bdeac1e3879197aa2fbe63484c
Nicolás Cardiel <cardiel@ucm.es>
2019-05-07 12:29:08 +0200

    Include link to github repository

--- repdoc/date_last_update.py
+++ repdoc/date_last_update.py
@@ -28,9 +28,11 @@ def date_last_update():
 
     return '''\n\n
 <br><br>
-<p style="color: #999; width: 100%; background-color: #ddd; 
+<p style="color: #000; width: 100%; background-color: #ddd; 
 text-align: center;">
 Last update: ''' + datetime_short() + '''\n
-&nbsp; &mdash; &nbsp; Created with RepDoc version ''' + version + '''
+&nbsp; &mdash; &nbsp; Created with 
+<a href="https://github.com/nicocardiel/repdoc">RepDoc</a>
+version ''' + version + '''
 \n&nbsp; &mdash; &nbsp; 
 Copyright &copy; 2019 Universidad Complutense de Madrid</p>\n\n'''
